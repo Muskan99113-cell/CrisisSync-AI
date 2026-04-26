e73f560e0b917b2e7f4b3dd5d2cb5e44eb10c3a0
Muskan Sharma <muccu484@gmail.com>
2026-04-26 13:02:38 +0530

    fixed firebase config for railway

--- firebase_config.py
+++ firebase_config.py
@@ -1,17 +1,20 @@
 import firebase_admin
 from firebase_admin import credentials, db
 import os
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# Firebase se connection
-cred = credentials.Certificate('serviceAccountKey.json')
+if os.getenv('GOOGLE_CREDENTIALS'):
+    cred_dict = json.loads(os.getenv('GOOGLE_CREDENTIALS'))
+    cred = credentials.Certificate(cred_dict)
+else:
+    cred = credentials.Certificate('serviceAccountKey.json')
 
 firebase_admin.initialize_app(cred, {
     'databaseURL': os.getenv('FIREBASE_URL')
 })
 
-# Yeh function database ka reference deta hai
 def get_db():
     return db
